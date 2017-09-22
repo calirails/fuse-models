eb510bc3cc74827c3ad48d4822c0b9ee097acda9
calvin <calvin98115@yahoo.com>
2017-09-22 00:09:04 -0700

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+var Observable = require('FuseJS/Observable');
+
+var appTitle = "Let's go get it!";
+
+module.exports = {
+    appCaption: appTitle
+};
---
+++ viewmodel.js
@@ -0,0 +1,7 @@
+var numbers = 1;
+var letters = 'abc';
+
+module.exports = {
+    numbers: numbers,
+    letters: letters
+};
